e3a21b4ebe35717e06316c700e75daae5a2f0064
nehapriya03 <56948455+nehapriya03@users.noreply.github.com>
2021-10-08 19:52:58 -0400

    [All order api done except update api]

--- Controller/OrderController.js
+++ Controller/OrderController.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const OrderStatus = require("../models/OrderModel");
 const orderRepository = require("../repository/OrderRepository");
+const orderModel = require("../models/OrderModel");
 
 exports.addOrder = async (req, res) => {
   let {
@@ -59,5 +60,168 @@ exports.addOrder = async (req, res) => {
     .catch((error) => {
       console.error(`There was an error while adding the order`, error);
       return res.status(500).send(`There was an error while adding the order`);
+    });
+};
+
+exports.getOrderById = async (req, res) => {
+  let { id } = req.params;
+  await orderRepository
+    .getOrderById(id)
+    .then((orderFound) => {
+      if (orderFound === 0) {
+        console.info(`No order with id: ${id} has been found.`);
+        return res.status(404).send(`No order with id: ${id} has been found.`);
+      }
+      console.log(`Order with orderId: ${id} has been successfully found.`);
+      return res.status(200).send(orderFound);
+      // return res.status(200).send(`ljmkhn`);
+    })
+    .catch((error) => {
+      console.error("There was an error.", error);
+      return res.status(500).send("There was an error.");
+    });
+};
+
+exports.getllOrder = async (req, res) => {
+  await orderRepository
+    .getAllOrders()
+    .then((orderFound) => {
+      if (orderFound === 0) {
+        console.info(`No order has been found.`);
+        return res.status(404).send(`No order has been found.`);
+      }
+      console.log(`Order has been successfully found.`);
+      return res.status(200).send(orderFound);
+      // return res.status(200).send(`ljmkhn`);
+    })
+    .catch((error) => {
+      console.error("There was an error.", error);
+      return res.status(500).send("There was an error.");
+    });
+};
+
+exports.getOrdersByUserId = async (req, res) => {
+  let { id } = req.params;
+  await orderRepository
+    .getOrdersByUserId(id)
+    .then((orderFound) => {
+      if (orderFound.length === 0) {
+        console.info(`No order has been found.`);
+        return res.status(404).send(`No order has been found.`);
+      }
+      console.log(`Order has been successfully found.`);
+      return res.status(200).send(orderFound);
+      // return res.status(200).send(`ljmkhn`);
+    })
+    .catch((error) => {
+      console.error("There was an error.", error);
+      return res.status(500).send("There was an error.");
+    });
+};
+
+exports.getOrdersByOrderStatus = async (req, res) => {
+  let status = req.body;
+  await orderRepository
+    .getOrdersByOrderStatus(status)
+    .then((orderFound) => {
+      if (orderFound.length === 0) {
+        console.info(`No order has been found.`);
+        return res.status(404).send(`No order has been found.`);
+      }
+      console.log(`Order has been successfully found.`);
+      return res.status(200).send(orderFound);
+      // return res.status(200).send(`ljmkhn`);
+    })
+    .catch((error) => {
+      console.error("There was an error.", error);
+      return res.status(500).send("There was an error.");
+    });
+};
+
+exports.getOrdersBySalespersonId = async (req, res) => {
+  let { id } = req.params;
+  await orderRepository
+    .getOrdersBySalespersonId(id)
+    .then((orderFound) => {
+      if (orderFound.length === 0) {
+        console.info(`No order has been found.`);
+        return res.status(404).send(`No order has been found.`);
+      }
+      console.log(`Order has been successfully found.`);
+      return res.status(200).send(orderFound);
+      // return res.status(200).send(`ljmkhn`);
+    })
+    .catch((error) => {
+      console.error("There was an error.", error);
+      return res.status(500).send("There was an error.");
+    });
+};
+
+exports.updateOrder = async (req, res) => {
+  let { id } = req.params;
+
+  let {
+    _id,
+    userId,
+    salesPersonId,
+    productQuantityList,
+    address,
+    productWarehouseCountList,
+    createdDate,
+    status,
+    paymentMode,
+  } = req.body;
+
+  if (_id !== id) {
+    console.error(`Id in the body and path must be same.`);
+    return res.status(400).send(`Id in the body and path must be same.`);
+  }
+
+  await orderRepository
+    .getOrderByIdSimple(_id)
+    .then(async (orderFound) => {
+      if (orderFound === null) {
+        console.info(`There is no order with id: ${_id} in the database.`);
+        return res
+          .status(404)
+          .send(`There is no order with id: ${_id} in the database.`);
+      }
+
+      let order = new OrderStatus({
+        _id,
+        userId,
+        salesPersonId,
+        productQuantityList,
+        address,
+        productWarehouseCountList,
+        createdDate,
+        status,
+        paymentMode,
+      });
+      await orderRepository
+        .updateOrder(order)
+        .then((orderUpdated) => {
+          if (orderUpdated.n === 0) {
+            console.error(`Order has not been updated. `);
+            return res.status(200).send(`Order has not been updated. `);
+          }
+          console.info(`order has been updated.`);
+          return res.status(400).send(` order has been updated.`);
+        })
+        .catch((error) => {
+          console.error(`There was an error in updating the order.`, error);
+          return res
+            .status(500)
+            .send(`There was an error in updating the order.`);
+        });
+    })
+    .catch((error) => {
+      console.error(
+        `There was an error in finding the order with id: ${id}.`,
+        error
+      );
+      return res
+        .status(500)
+        .send(`There was an error in finding the order with id: ${id}.`);
     });
 };
--- repository/OrderRepository.js
+++ repository/OrderRepository.js
@@ -12,10 +12,52 @@ exports.addOrder = async (order) => {
 
 exports.getOrderById = async (id) => {
   try {
-    await Order.aggregate([
+    return await Order.aggregate([
       {
-        $match: { _id: mongoose.Types.ObjectId(id) },
+        $match: {
+          _id: mongoose.Types.ObjectId(id),
+        },
       },
+      {
+        $lookup: {
+          from: "users",
+          localField: "userId",
+          foreignField: "_id",
+          as: "user",
+        },
+      },
+      {
+        $set: {
+          user: {
+            $first: "$user",
+          },
+        },
+      },
+      {
+        $lookup: {
+          from: "salespeople",
+          localField: "salesPersonId",
+          foreignField: "_id",
+          as: "salesperson",
+        },
+      },
+      {
+        $set: {
+          salesperson: {
+            $first: "$salesperson",
+          },
+        },
+      },
+    ]);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+exports.getAllOrders = async () => {
+  try {
+    return await Order.aggregate([
       {
         $lookup: {
           from: "users",
@@ -49,15 +91,84 @@ exports.getOrderById = async (id) => {
   }
 };
 
-// exports.getAllOrders = async () => {
-//   try {
-//     return await Order.aggregate([
-//         {
-//             $lookup:
-//             {
+exports.getOrdersByUserId = async (id) => {
+  try {
+    return await Order.findOne({ userId: id }).sort({ createdDate: -1 });
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
 
-//             }
-//         }
-//     ]);
-//   } catch (error) {}
-// };
+exports.getOrdersByOrderStatus = async (status) => {
+  try {
+    return await Order.aggregate([
+      {
+        $match: { status: status },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "userId",
+          foreignField: "_id",
+          as: "user",
+        },
+      },
+      {
+        $sort: {
+          createdDate: -1,
+        },
+      },
+    ]);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+exports.getOrdersBySalespersonId = async (id) => {
+  try {
+    return await Order.aggregate([
+      {
+        $match: { salesPersonId: mongoose.Types.ObjectId(id) },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "userId",
+          foreignField: "_id",
+          as: "user",
+        },
+      },
+      {
+        $set: { user: { $first: "$user" } },
+      },
+      {
+        $sort: {
+          createdDate: -1,
+        },
+      },
+    ]);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+exports.updateOrder = async (order) => {
+  try {
+    return await Order.updateOne({ _id: order._id }, { $set: order });
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+exports.getOrderByIdSimple = async (id) => {
+  try {
+    return await Order.findOne({ _id: id });
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
